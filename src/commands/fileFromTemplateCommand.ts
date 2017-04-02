d3731980123709bd133e420293dca1f6fde9104a
Nate Taylor <taylonr@gmail.com>
2017-04-02 17:30:42 -0500

    Fixing linting error.

--- src/commands/fileFromTemplateCommand.ts
+++ src/commands/fileFromTemplateCommand.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-import vscode = require('vscode');
+import vscode = require("vscode");
 import fs = require("fs");
 import path = require("path");
 import TemplatesManager from "../templatesManager";
