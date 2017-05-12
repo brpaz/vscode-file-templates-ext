a0e5f14ed6ca717f932edd24b4f8f5d306401c63
Li Theen Kok <litheen@apptivitylab.com>
2017-05-12 20:35:32 +0800

    Improvements search and replace; show input box if variable not found in settings

--- src/commands/fileFromTemplateCommand.ts
+++ src/commands/fileFromTemplateCommand.ts
@@ -24,7 +24,7 @@ export function run(templatesManager: TemplatesManager, args: any) {
     let targetFolder = args ? args.fsPath : vscode.workspace.rootPath;
 
     if (templates.length === 0) {
-        let optionGoToTemplates = <vscode.MessageItem> {
+        let optionGoToTemplates = <vscode.MessageItem>{
             title: "Open Templates Folder"
         };
 
@@ -51,7 +51,7 @@ export function run(templatesManager: TemplatesManager, args: any) {
         }
 
         // ask for filename
-        let inputOptions = <vscode.InputBoxOptions> {
+        let inputOptions = <vscode.InputBoxOptions>{
             prompt: "Please enter the desired file name",
             value: selection,
         };
@@ -63,46 +63,68 @@ export function run(templatesManager: TemplatesManager, args: any) {
             const className = filename.replace(/\.[^/.]+$/, "");
             const resultsPromise = [];
 
-            let regexResult = /#{(\w+)}/g.exec(fileContents);
-            while (regexResult) {
-                const variableName = regexResult[1];
-                const regex = new RegExp(`#{${variableName}}`, "g");
+            let expression = /#{(\w+)}/g;
+
+            var matches;
+            var placeholders = [];
+            while (matches = expression.exec(fileContents)) {
+                if (placeholders.indexOf(matches[0]) != -1) {
+                    continue;
+                }
+                placeholders.push(matches[0]);
+            }
+
+            placeholders.forEach(function (placeholder) {
+                const variableName = /#{(\w+)}/.exec(placeholder)[1];
+                const search = new RegExp(placeholder, 'g')
 
                 switch (variableName) {
                     case "filename":
-                        fileContents = fileContents.replace(regex, className);
+                        fileContents = fileContents.replace(search, className);
                         break;
-
                     case "filepath":
                         let workspaceRoot = vscode.workspace.rootPath;
-                        fileContents = fileContents.replace(regex, targetFolder.replace(`${workspaceRoot}/`, ""));
+                        fileContents = fileContents.replace(search, targetFolder.replace(`${workspaceRoot}/`, ""));
                         break;
-
                     case "year":
-                        fileContents = fileContents.replace(regex, moment().format("YYYY"));
+                        fileContents = fileContents.replace(search, moment().format("YYYY"));
                         break;
-
                     case "date":
-                        fileContents = fileContents.replace(regex, moment().format("D MMM YYYY"));
+                        fileContents = fileContents.replace(search, moment().format("D MMM YYYY"));
                         break;
-
                     default:
                         if (workspaceSettings && workspaceSettings[variableName]) {
-                            fileContents = fileContents.replace(regex, workspaceSettings[variableName]);
+                            fileContents = fileContents.replace(search, workspaceSettings[variableName]);
                         } else {
-                            fileContents = fileContents.replace(regex, variableName.toUpperCase());
+                            let variableInput = <vscode.InputBoxOptions>{
+                                prompt: `Please enter the desired value for "${variableName}"`
+                            };
+                            let variablePromise = new Promise((resolve, reject) => {
+                                vscode.window.showInputBox(variableInput).then(value => {
+                                    let replacement;
+                                    if (!value) {
+                                        replacement = variableName.toUpperCase();
+                                    } else {
+                                        replacement = value;
+                                    }
+
+                                    fileContents = fileContents.replace(search, replacement);
+                                    resolve(fileContents);
+                                });
+                            });
+                            resultsPromise.push(variablePromise);
                         }
                         break;
                 }
+            });
 
-                regexResult = /#{(\w+)}/g.exec(fileContents);
-            }
-
-            fs.writeFile(path.join(targetFolder, filename), fileContents, function (err) {
-                if (err) {
-                    vscode.window.showErrorMessage(err.message);
-                }
-                vscode.window.showInformationMessage(filename + " created");
+            Promise.all(resultsPromise).then(() => {
+                fs.writeFile(path.join(targetFolder, filename), fileContents, function (err) {
+                    if (err) {
+                        vscode.window.showErrorMessage(err.message);
+                    }
+                    vscode.window.showInformationMessage(filename + " created");
+                });
             });
         });
     });
